46e294c0f0bee5940ce5cba946d0d76830587317
Dušan Janeček <dusanjanecek@dusans-macbook-pro.local>
2019-02-14 23:34:12 +0100

    Initial Commit

---
+++ Pokedex3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex3
+//
+//  Created by Dušan Janeček on 14/02/2019.
+//  Copyright © 2019 Dušan Janeček. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
