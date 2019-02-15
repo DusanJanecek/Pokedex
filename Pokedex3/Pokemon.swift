f4bbaafaa2ea5a7f7fc498b3093340b20fe65132
Dušan Janeček <dusanjanecek@lanmgmt-dev.druzba.uniba.sk>
2019-02-15 01:12:38 +0100

    UI of a main view

--- Pokedex3/Pokemon.swift
+++ Pokedex3/Pokemon.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    var pokedexId: Int {
+        get {
+            return _pokedexId
+        }
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
