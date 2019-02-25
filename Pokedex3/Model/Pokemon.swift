e1cb456a5e6d5f1282401d68c7b2637a17ca290e
Dušan Janeček <dusanjanecek@lanmgmt-dev.druzba.uniba.sk>
2019-02-25 11:57:40 +0100

    feat: Update Model

--- Pokedex3/Model/Pokemon.swift
+++ Pokedex3/Model/Pokemon.swift
@@ -19,57 +19,91 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolution: String!
+    private var _urlAddress: String!
+    
+    var urlAddress: String {
+        get {
+            return _urlAddress
+        }
+    }
     
     var name: String {
         get {
+            if _name == nil {
+                _name = ""
+            }
             return _name
         }
     }
     
     var pokedexId: Int {
         get {
+            if _pokedexId == nil {
+                _pokedexId = -1
+            }
             return _pokedexId
         }
     }
     
     var description: String {
         get {
+            if _description == nil {
+                _description = ""
+            }
             return _description
         }
     }
     
     var type: String {
         get {
+            if _type == nil {
+                _type = ""
+            }
             return _type
         }
     }
     
     var defense: String {
         get {
+            if _defense == nil {
+                _defense = ""
+            }
             return _defense
         }
     }
     
     var height: String {
         get {
+            if _height == nil {
+                _height = ""
+            }
             return _height
         }
     }
     
     var weight: String {
         get {
+            if _weight == nil {
+                _weight = ""
+            }
             return _weight
         }
     }
     
     var attack: String {
         get {
+            if _attack == nil {
+                _attack = ""
+            }
             return _attack
         }
     }
     
     var nextEvolution: String {
         get {
+            if _nextEvolution == nil {
+                _nextEvolution = ""
+            }
             return _nextEvolution
         }
     }
@@ -77,6 +111,7 @@ class Pokemon {
     init(name: String, pokedexId: Int) {
         self._name = name
         self._pokedexId = pokedexId
+        self._urlAddress = "\(URL_BASE)\(URL_POKEMON)\(self.pokedexId)"
     }
     
 }
---
+++ Pokedex3/Utility/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Pokedex3
+//
+//  Created by Dušan Janeček on 25/02/2019.
+//  Copyright © 2019 Dušan Janeček. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co"
+let URL_POKEMON = "/api/v2/pokemon/"
+
+typealias DownloadCompleted = () -> ()
+
+
