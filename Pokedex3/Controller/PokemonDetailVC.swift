15b26bed647692b1af445688c2efa1410cd6a16f
Dušan Janeček <dusanjanecek@lanmgmt-dev.druzba.uniba.sk>
2019-02-26 23:33:57 +0100

    feat: Finished PokeAPI parsing  and updated model

--- Pokedex3/Controller/PokemonDetailVC.swift
+++ Pokedex3/Controller/PokemonDetailVC.swift
@@ -21,7 +21,13 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var attackLabel: UILabel!
     @IBOutlet weak var evoLabel: UILabel!
     @IBOutlet weak var currentEvoImage: UIImageView!
-    @IBOutlet weak var nextEvoLabel: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var bottomStackView: UIStackView!
+    
+    private var _firstId: Int!
+    private var _secondId: Int = -1
+    private var _thirdId: Int = -1
+
     
     private var _pokemon: Pokemon!
     
@@ -37,7 +43,78 @@ class PokemonDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        nameLabel.text = _pokemon.name
+        
+        
+        // Download data from API and update screen after downloading
+        pokemon.downloadPokemonDetail {
+            self.setEvolutions()
+            self.updateUI()
+        }
+        
+    }
+    
+    func updateUI() {
+        heightLabel.text = pokemon.height
+        weightLabel.text = pokemon.weight
+        defenseLabel.text = pokemon.defense
+        attackLabel.text = pokemon.attack
+        pokedexIdLabel.text = "\(pokemon.pokedexId)"
+        typeLabel.text = pokemon.type
+        descLabel.text = pokemon.description
+        
+        
+        if self._secondId == -1 || self._thirdId == -1 {
+            
+            evoLabel.text = "There is only one evolution"
+            
+            if self._secondId == -1 && self._thirdId == -1 {
+                evoLabel.text = "The are no evolutions"
+            }
+            
+        } else {
+            
+            evoLabel.text = "The other evolutions"
+        }
+        
+        if self._firstId != -1 {
+            mainImage.image = UIImage(named: "\(self._firstId!)")
+        }
+        if self._secondId != -1 {
+            currentEvoImage.image = UIImage(named: "\(self._secondId)")
+        }
+        if self._thirdId != -1 {
+            nextEvoImage.image = UIImage(named: "\(self._thirdId)")
+        }
+        
+    }
+    
+    func setEvolutions() {
+        
+        switch pokemon.pokedexId {
+        case pokemon.firstEvolutionId:
+            self._firstId = pokemon.firstEvolutionId
+            self._secondId = pokemon.secondEvolutionId
+            self._thirdId = pokemon.thirdEvolutionId
+            nameLabel.text = pokemon.firstEvolutionName.capitalized
+            break
+        case pokemon.secondEvolutionId:
+            self._firstId = pokemon.secondEvolutionId
+            self._secondId = pokemon.firstEvolutionId
+            self._thirdId = pokemon.thirdEvolutionId
+            nameLabel.text = pokemon.secondEvolutionName.capitalized
+
+            break
+        case pokemon.thirdEvolutionId:
+            self._firstId = pokemon.thirdEvolutionId
+            self._secondId = pokemon.firstEvolutionId
+            self._thirdId = pokemon.secondEvolutionId
+            nameLabel.text = pokemon.thirdEvolutionName.capitalized
+            
+            break
+        default:
+            print("Error")
+        }
+        
     }
     
     @IBAction func backPressed(_ sender: Any) {
@@ -45,4 +122,15 @@ class PokemonDetailVC: UIViewController {
     }
     
 
+    @IBAction func changeSecEvo(_ sender: Any) {
+        self._pokemon = Pokemon(name: pokemon.secondEvolutionName, pokedexId: _secondId)
+        self.viewDidLoad()
+    }
+    
+    @IBAction func changeThirdEvo(_ sender: Any) {
+        self._pokemon = Pokemon(name: pokemon.thirdEvolutionName, pokedexId: _thirdId)
+        self.viewDidLoad()
+    }
+    
+    
 }
--- Pokedex3/Model/Pokemon.swift
+++ Pokedex3/Model/Pokemon.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Alamofire
 
 class Pokemon {
     
@@ -18,8 +19,78 @@ class Pokemon {
     private var _height: String!
     private var _weight: String!
     private var _attack: String!
-    private var _nextEvolution: String!
+    private var _firstEvolution: String!
+    private var _firstEvolutionId: Int!
+    private var _firstEvolutionName: String!
+    private var _evolutionLvl: String!
     private var _urlAddress: String!
+    private var _secondEvolutionId: Int!
+    private var _secondEvolutionName: String!
+    private var _thirdEvolutionId: Int!
+    private var _thirdEvolutionName: String!
+    
+    var thirdEvolutionId: Int {
+        get {
+            if _thirdEvolutionId == nil {
+                _thirdEvolutionId = -1
+            }
+            return _thirdEvolutionId
+        }
+    }
+    
+    var thirdEvolutionName: String {
+        get {
+            if _thirdEvolutionName == nil {
+                _thirdEvolutionName = ""
+            }
+            return _thirdEvolutionName
+        }
+    }
+    
+    var secondEvolutionName: String {
+        get {
+            if _secondEvolutionName == nil {
+                _secondEvolutionName = ""
+            }
+            return _secondEvolutionName
+        }
+    }
+    
+    var secondEvolutionId: Int {
+        get {
+            if _secondEvolutionId == nil {
+                _secondEvolutionId = -1
+            }
+            return _secondEvolutionId
+        }
+    }
+    
+    var firstEvolutionId: Int {
+        get {
+            if _firstEvolutionId == nil {
+                _firstEvolutionId = -1
+            }
+            return _firstEvolutionId
+        }
+    }
+    
+    var firstEvolutionName: String {
+        get {
+            if _firstEvolutionName == nil {
+                _firstEvolutionName = ""
+            }
+            return _firstEvolutionName
+        }
+    }
+    
+    var evolutionLvl: String {
+        get {
+            if _evolutionLvl == nil {
+                _evolutionLvl = ""
+            }
+            return _evolutionLvl
+        }
+    }
     
     var urlAddress: String {
         get {
@@ -101,17 +172,224 @@ class Pokemon {
     
     var nextEvolution: String {
         get {
-            if _nextEvolution == nil {
-                _nextEvolution = ""
+            if _firstEvolution == nil {
+                _firstEvolution = ""
             }
-            return _nextEvolution
+            return _firstEvolution
         }
     }
     
     init(name: String, pokedexId: Int) {
         self._name = name
         self._pokedexId = pokedexId
-        self._urlAddress = "\(URL_BASE)\(URL_POKEMON)\(self.pokedexId)"
+        self._urlAddress = "\(URL_BASE)\(URL_POKEMON)\(self.pokedexId)/"
+    }
+    
+    func downloadPokemonDetail(completed: @escaping DownloadCompleted) {
+        
+        Alamofire.request(self._urlAddress).responseJSON { (response) in
+            
+            if let json = response.result.value as? Dictionary<String, Any> {
+                
+                if let height = json["height"] as? Int {
+                    self._height = "\(height)"
+                }
+                
+                if let weight = json["weight"] as? Int {
+                    self._weight = "\(weight)"
+                }
+                
+                if let types = json["types"] as? [Dictionary<String, Any>], types.count > 0 {
+                    
+                    for i in 0..<types.count {
+                        
+                        if let typeStr = types[i]["type"] as? Dictionary<String, Any> {
+                            
+                            if let type = typeStr["name"] as? String {
+                                if i < 1 {
+                                    self._type = "\(type.capitalized)"
+                                } else {
+                                    self._type += "/\(type.capitalized)"
+                                    if i != types.count-1 {
+                                        self._type += "/"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                if let desc_uri = json["species"] as? Dictionary<String, Any> {
+                    var index = -1
+                    
+                    if let url = desc_uri["url"] as? String {
+                        
+                        Alamofire.request(url).responseJSON(completionHandler: { (response) in
+                            
+                            if let descriptionJSON = response.result.value as? Dictionary<String, Any> {
+                                
+                                if let textEntries = descriptionJSON["flavor_text_entries"] as? [Dictionary<String, Any>], textEntries.count > 0 {
+                                    
+                                    for i in 0..<textEntries.count {
+                                        
+                                        if let langu = textEntries[i]["language"] as? Dictionary<String, Any> {
+                                            
+                                            if let language = langu["name"] as? String {
+                                                
+                                                if language == "en" {
+                                                    index = i
+                                                    break
+                                                }
+                                            }
+                                        }
+                                    }
+                                    
+                                    if index != -1 {
+                                        
+                                        if let description = textEntries[index]["flavor_text"] as? String {
+                                            self._description = description.replacingOccurrences(of: "POKMON", with: "Pokemon")
+                                        }
+                                        
+                                    }
+                                    
+                                }
+                                
+                                if let evo_chain = descriptionJSON["evolution_chain"] as? Dictionary<String, Any> {
+                                    
+                                    if let evo_url = evo_chain["url"] as? String {
+                                        
+                                        Alamofire.request(evo_url).responseJSON(completionHandler: { (response) in
+                                            
+                                            if let evoJSON = response.result.value as? Dictionary<String, Any> {
+                                                
+                                                // First Evo ID
+                                                if let chain = evoJSON["chain"] as? Dictionary<String, Any> {
+                                                    
+                                                    if let species = chain["species"] as? Dictionary<String, Any> {
+                                                        
+                                                        if let firstEvo = species["url"] as? String {
+                                                            let firstEvoId = firstEvo.replacingOccurrences(of: "https://pokeapi.co/api/v2/pokemon-species/", with: "").replacingOccurrences(of: "/", with: "")
+                                                            self._firstEvolutionId = Int(firstEvoId)
+                                                        }
+                                                        
+                                                        if let firstEvoName = species["name"] as? String {
+                                                            self._firstEvolutionName = firstEvoName
+                                                        }
+                                                        
+                                                    }
+                                                    
+                                                    // Second Evo ID
+                                                    
+                                                    if let evoTo = chain["evolves_to"] as? [Dictionary<String, Any>], evoTo.count > 0 {
+                                                        
+                                                        for i in 0..<evoTo.count {
+                                                            
+                                                            if let firstElement = evoTo[0] as? Dictionary<String, Any> {
+                                                                
+                                                                if let species = firstElement["species"] as? Dictionary<String, Any> {
+                                                                    
+                                                                    if let secondEvo = species["url"] as? String {
+                                                                        let secondEvoId = secondEvo.replacingOccurrences(of: "https://pokeapi.co/api/v2/pokemon-species/", with: "").replacingOccurrences(of: "/", with: "")
+                                                                        self._secondEvolutionId = Int(secondEvoId)
+                                                                    }
+                                                                    
+                                                                    if let secondEvoName = species["name"] as? String {
+                                                                        self._secondEvolutionName = secondEvoName
+                                                                    }
+                                                                    
+                                                                }
+                                                                
+                                                            }
+                                                            
+                                                            // Third Evo ID
+                                                            
+                                                            if evoTo.count > 1 {
+                                                                
+                                                                if let thirdElement = evoTo[1] as? Dictionary<String, Any> {
+                                                                    
+                                                                    if let species = thirdElement["species"] as? Dictionary<String, Any> {
+                                                                        
+                                                                        if let thirdEvo = species["url"] as? String {
+                                                                            let thirdEvoId = thirdEvo.replacingOccurrences(of: "https://pokeapi.co/api/v2/pokemon-species/", with: "").replacingOccurrences(of: "/", with: "")
+                                                                            self._thirdEvolutionId = Int(thirdEvoId)
+                                                                        }
+                                                                        
+                                                                        if let thirdEvoName = species["name"] as? String {
+                                                                            self._thirdEvolutionName = thirdEvoName
+                                                                        }
+                                                                        
+                                                                    }
+                                                                    
+                                                                }
+                                                                
+                                                            } else {
+                                                                
+                                                                if let thirdEvoTo = evoTo[0]["evolves_to"] as? [Dictionary<String, Any>], thirdEvoTo.count > 0 {
+                                                                    
+                                                                    if let species = thirdEvoTo[0]["species"] as? Dictionary<String, Any> {
+                                                                        
+                                                                        if let thirdEvo = species["url"] as? String {
+                                                                            let thirdEvoId = thirdEvo.replacingOccurrences(of: "https://pokeapi.co/api/v2/pokemon-species/", with: "").replacingOccurrences(of: "/", with: "")
+                                                                            self._thirdEvolutionId = Int(thirdEvoId)
+                                                                        }
+                                                                        
+                                                                        if let thirdEvoName = species["name"] as? String {
+                                                                            self._thirdEvolutionName = thirdEvoName
+                                                                        }
+                                                                        
+                                                                    }
+                                                                    
+                                                                }
+                                                                
+                                                            }
+                                                            
+                                                        }
+                                                        
+                                                    }
+                                                    
+                                                    
+                                                }
+                                                
+                                            }
+                                            completed()
+                                        })
+                                        
+                                    }
+                                    
+                                }
+                                
+                            }
+//                           completed()
+                        })
+                        
+                    }
+                    
+                }
+                
+                if let stats = json["stats"] as? [Dictionary<String, Any>], stats.count > 0 {
+                    
+                    guard stats.count >= 4 else {
+                        return
+                    }
+                    
+                    if let attack = stats[3]["base_stat"] as? Int {
+                        self._attack = "\(attack)"
+                    }
+                    
+                    guard stats.count >= 5 else {
+                        return
+                    }
+                    
+                    if let defense = stats[4]["base_stat"] as? Int {
+                        self._defense = "\(defense)"
+                    }
+
+                }
+
+            }
+            
+//          completed()
+        }
     }
     
 }
